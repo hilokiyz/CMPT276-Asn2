fe59a5d3f440d6a27fba7bcc6c4fbd0ec5f5f363
hilokiyz <74848293+hilokiyz@users.noreply.github.com>
2023-06-06 09:56:05 -0700

    Init project

---
+++ src/test/java/sfu/cmpt276/asn2/Asn2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sfu.cmpt276.asn2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Asn2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
